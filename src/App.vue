edf503b7a3fb2a331223cfc4b291212e7af45514
OdongAlican <sandieo.2020@gmail.com>
2021-01-28 18:03:54 +0300

    Add the following - Update App.vue component - Remove Hello world component - Add store to main.js file

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="addcutomer-section">
+        <AddCustomer/>
+      </div>
+      <Customers/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Customers from './components/Customers.vue'
+import AddCustomer from './components/AddCustomer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Customers,
+    AddCustomer
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
