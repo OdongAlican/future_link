69d8b38694f493aca172e408ad6e9ade7d381a9a
OdongAlican <sandieo.2020@gmail.com>
2021-01-28 18:00:43 +0300

    Update README

---
+++ src/components/Customers.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="cutomer-section">
+        <h3 class="header-section">Customers</h3>
+        <div class="customers-list">
+            <div v-for="customer in allCustomers" :key="customer.id" class="customer card p-2 m-2">
+                <p>{{ customer.name }}</p>
+                <p>{{ customer.address}}</p>
+                <div class="avatar">
+                    <img :src="customer.avatar">
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import { mapGetters, mapActions } from 'vuex';
+export default {
+    name: "Customers",
+    methods: {
+        ...mapActions(['fetchCustomers'])
+    },
+    computed: mapGetters(['allCustomers']),
+    created(){
+        this.fetchCustomers();
+    }
+}
+</script>
+<style scoped>
+
+</style>
