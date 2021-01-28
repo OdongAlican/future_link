110d0a947525f5bae7caffd7c93ea7df0a15d5b1
OdongAlican <sandieo.2020@gmail.com>
2021-01-28 18:03:04 +0300

    add addCustomer component

---
+++ src/components/AddCustomer.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="add-customer-section">
+        <h3>Add Customer</h3>
+        <div class="add-customer-form">
+            <form>
+                
+            </form>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: "AddCustomer"
+}
+</script>
+<style scoped>
+
+</style>
