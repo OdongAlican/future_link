c9517fc3cb311fefe35da621efcb3a99a4654e49
OdongAlican <sandieo.2020@gmail.com>
2021-02-03 22:43:12 +0300

    add Invoices component

---
+++ src/components/Invoices.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="invoices-list">
+        <p>List of invoices</p>
+        <div class="container-invoice card" v-for="invoice in allInvoices" :key="invoice.id">
+            <p>{{ invoice.invoice_no }}</p>
+            <p>{{ invoice.due_date }}</p>
+        </div>
+    </div>
+</template>
+<script>
+import { mapActions, mapGetters } from 'vuex'
+export default {
+    name: "Invoices",
+    methods: {
+        ...mapActions(['fetchInvoices'])
+    },
+    computed: mapGetters(['allInvoices']),
+    created(){
+    this.fetchInvoices(this.$route.params.id);
+    }
+}
+</script>
+<style scoped>
+
+</style>
