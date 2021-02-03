6117d0de6e78ab37d15f6dab144e390dbb480c3e
OdongAlican <sandieo.2020@gmail.com>
2021-02-03 23:07:53 +0300

    update the asyn requests with try catch block

--- src/store/modules/customers.js
+++ src/store/modules/customers.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const state = {
-    customers: [
-    ]
+    customers: []
 };
 
 const getters = {
@@ -11,13 +10,44 @@ const getters = {
 
 const actions = {
     async fetchCustomers({ commit }){
-        const response = await axios.get('https://6012bf0f54044a00172dc953.mockapi.io/sp5/api/v1/customer')
-        commit('setCustomers', response.data.items)
+        try {
+            const response = await axios.get('https://6012bf0f54044a00172dc953.mockapi.io/sp5/api/v1/customer')
+            commit('setCustomers', response.data.items)
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    async addCustomers({ commit }, data ){
+        try {
+            const response = await axios.post('https://6012bf0f54044a00172dc953.mockapi.io/sp5/api/v1/customer', data)
+            commit('newCustomer', response.data)            
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    async deleteCustomer({ commit }, id){
+        try {
+            await axios.delete(`https://6012bf0f54044a00172dc953.mockapi.io/sp5/api/v1/customer/${id}`)
+            commit('removeCustomer', id)            
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    async editCustomer({ commit }, id){
+        try {
+            const response = await axios.put(`https://6012bf0f54044a00172dc953.mockapi.io/sp5/api/v1/customer/${id}`, data)
+            console.log(response.data, 'updated customer')
+            commit('updateCustomer', response.data)            
+        } catch (error) {
+            console.log(error)
+        }
     }
 };
 
 const mutations = {
-    setCustomers: (state, customers) => (state.customers = customers)
+    setCustomers: (state, customers) => (state.customers = customers),
+    newCustomer: (state, customer) => state.customers.unshift(customer),
+    removeCustomer: (state, id) => state.customers = state.customers.filter(customer => customer.id !== id)
 };
 
 export default {
