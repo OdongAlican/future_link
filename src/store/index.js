a66d5332eb14bf26518c95f190494b9db31e6cdf
OdongAlican <sandieo.2020@gmail.com>
2021-02-03 23:07:02 +0300

    add invoices module

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import Vuex from 'vuex';
 import Vue from 'vue';
 import customers from './modules/customers';
+import invoices from './modules/invoices';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     modules: {
-        customers
+        customers,
+        invoices
     }
 });
