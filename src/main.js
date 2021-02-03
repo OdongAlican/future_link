5761cc01fad9ce0e60f05dca52ae51d63e7a5bde
OdongAlican <sandieo.2020@gmail.com>
2021-02-03 23:06:27 +0300

    configure vue router

--- src/main.js
+++ src/main.js
@@ -1,10 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
+import VueRouter from 'vue-router'
+import Customers from './components/Customers'
+import AddCustomer from './components/AddCustomer'
+import Invoices from './components/Invoices'
+
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
+const router = new VueRouter({
+  routes: [
+    { path: '/', component: Customers },
+    { path: '/add-customer', component: AddCustomer },
+    { path: '/customer/:id/invoices', component: Invoices },
+  ],
+  mode: 'history'
+})
+
 new Vue({
   store,
+  router,
   render: h => h(App),
 }).$mount('#app')
