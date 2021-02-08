d6ac967b0e7e92447b093f1cf42f89cc2cf61a13
OdongAlican <sandieo.2020@gmail.com>
2021-02-09 01:02:31 +0300

    remove create customer route

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue'
 import store from './store'
 import VueRouter from 'vue-router'
 import Customers from './components/Customers'
-import AddCustomer from './components/AddCustomer'
 import Invoices from './components/Invoices'
 
 Vue.use(VueRouter)
@@ -13,7 +12,6 @@ Vue.config.productionTip = false
 const router = new VueRouter({
   routes: [
     { path: '/', component: Customers },
-    { path: '/add-customer', component: AddCustomer },
     { path: '/customer/:id/invoices', component: Invoices },
   ],
   mode: 'history'
