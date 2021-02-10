9a8eb2c4d0788f9acc9888e525f0085895a0e8c8
OdongAlican <sandieo.2020@gmail.com>
2021-02-10 03:03:30 +0300

    Add the following - Add addInvoice action and respective mutation - Add editInvoice action and respective mutation - Add deleteInvoice action and respective mutation

--- src/store/modules/invoices.js
+++ src/store/modules/invoices.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const state = {
-    invoices: [
-    ]
+    invoices: []
 };
 
 const getters = {
@@ -18,10 +17,49 @@ const actions = {
             console.log(error, 'error messages')
         }
     },
+    async addInvoice({ commit }, data ){
+        try {
+            const response = await axios.post(`https://6012bf0f54044a00172dc953.mockapi.io/sp5/api/v1/customer/${data.customerId}/invoice`, data)
+            commit('newInvoice', response.data)            
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    async editInvoice({commit}, data) {
+        const details = {
+            due_date: data.due_date,
+            invoice_no: data.invoice_no
+        }
+
+        try {
+            const response = await axios.put(`https://6012bf0f54044a00172dc953.mockapi.io/sp5/api/v1/customer/${data.customerId}/invoice/${data.id}`, details)
+            commit('updateInvoice', response.data)   
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    async deleteInvoice({ commit }, data){
+        try {
+            await axios.delete(`https://6012bf0f54044a00172dc953.mockapi.io/sp5/api/v1/customer/${data.customerId}/invoice/${data.invoiceId}`)
+            commit('removeInvoice', data.invoiceId)            
+        } catch (error) {
+            console.log(error)
+        }
+    },
 };
 
 const mutations = {
     setInvoices: (state, invoices) => (state.invoices = invoices),
+    newInvoice: (state, invoice) => state.invoices.push(invoice),
+    removeInvoice: (state, id) => state.invoices = state.invoices.filter(invoice => invoice.id !== id),
+    updateInvoice: (state, updatedInvoice) => {
+        let invoices = [...state.invoices];
+        const index = invoices.findIndex(invoice => invoice.id === updatedInvoice.id);
+        if(index !== -1) {
+            invoices.splice(index, 1, updatedInvoice);
+        }
+        state.invoices = [...invoices];
+    }
 };
 
 export default {
