7f1ce0c81f3f8e4cc54432b39e7001fd10ee1bc9
OdongAlican <sandieo.2020@gmail.com>
2021-02-10 03:00:55 +0300

    import products module

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,14 @@ import Vuex from 'vuex';
 import Vue from 'vue';
 import customers from './modules/customers';
 import invoices from './modules/invoices';
+import products from './modules/products'
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     modules: {
         customers,
-        invoices
+        invoices,
+        products
     }
 });
